f1500a07cd503886f3e599092c282b2d5587ff4c
Kevin Feichtinger <kevin.feichtinger@jku.at>
2023-08-16 13:32:53 +0200

    Updated pprdsl reader/writer

--- src/main/java/at/jku/cps/travart/plugin/ppr/dsl/io/PprDslReader.java
+++ src/main/java/at/jku/cps/travart/plugin/ppr/dsl/io/PprDslReader.java
@@ -1,13 +1,18 @@
 /*******************************************************************************
  * TODO: explanation what the class does
- *  
+ *
  *  @author Kevin Feichtinger
- *  
+ *
  * Copyright 2023 Johannes Kepler University Linz
  * LIT Cyber-Physical Systems Lab
  * All rights reserved
  *******************************************************************************/
 package at.jku.cps.travart.plugin.ppr.dsl.io;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
 
 import at.jku.cps.travart.core.common.IReader;
 import at.jku.cps.travart.core.exception.NotSupportedVariabilityTypeException;
@@ -15,18 +20,20 @@ import at.sqi.ppr.dsl.reader.DslReader;
 import at.sqi.ppr.dsl.reader.exceptions.DslParsingException;
 import at.sqi.ppr.model.AssemblySequence;
 
-import java.io.IOException;
-import java.nio.file.Path;
-
 public class PprDslReader implements IReader<AssemblySequence> {
 
-    @Override
-    public AssemblySequence read(final Path path) throws IOException, NotSupportedVariabilityTypeException {
-        final DslReader dslReader = new DslReader();
-        try {
-            return dslReader.readDsl(path.toString());
-        } catch (final DslParsingException e) {
-            throw new NotSupportedVariabilityTypeException(e);
-        }
-    }
+	@Override
+	public AssemblySequence read(final Path path) throws IOException, NotSupportedVariabilityTypeException {
+		final DslReader dslReader = new DslReader();
+		try {
+			return dslReader.readDsl(path.toString());
+		} catch (final DslParsingException e) {
+			throw new NotSupportedVariabilityTypeException(e);
+		}
+	}
+
+	@Override
+	public Iterable<String> fileExtensions() {
+		return Collections.unmodifiableList(List.of(".dsl"));
+	}
 }
